e1273fa66bb1d40d83ce2e6d00382490fc4cf117
amod kumar <amodkumar968@gmail.com>
2023-10-07 12:36:18 +0530

    Added backend

--- infl-65210185df00051c5743745e.tf
+++ infl-65210185df00051c5743745e.tf
@@ -1,4 +1,12 @@
 module "terraform-aws-s3-bucket" {
   source = "github.com/mineiros-io/terraform-aws-s3-bucket"
+  versioning = true
+}
 
+terraform {
+  backend "s3" {
+    bucket = "834277767436-tf-state"
+    key    = "ecr.tfstate"
+    region = "us-east-1"
+  }
 }
